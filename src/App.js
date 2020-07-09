7146e1220a619381053045025b07b960a03ad506
daproclaima <sn.itpro@gmail.com>
2020-07-10 00:16:28 +0200

    modifies App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <Fragment>
-      <div className="App">
-        <h1>React application</h1>
-      </div>
-      <h2>Second title</h2>
-    </Fragment>
-  );
+class App extends Component {
+  render () {
+    return (
+      <Fragment>
+        <div className="App">
+          <h1>React application</h1>
+        </div>
+        <h2>Second title</h2>
+      </Fragment>
+    )
+  }
 }
 
 export default App;
