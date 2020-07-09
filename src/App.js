83b00fa1edfe6484f2cb1b3a025f0c5eeb384a05
daproclaima <sn.itpro@gmail.com>
2020-07-10 00:02:35 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <h1>React application</h1>
+      </div>
+      <h2>Second title</h2>
+    </Fragment>
   );
 }
 
